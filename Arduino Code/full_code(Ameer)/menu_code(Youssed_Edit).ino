77c2f86e61519ec1d54c7f1d0cb1e30adf088592
ameerlouly <ameer.Louly04@eng-st.cu.edu.eg>
2024-10-30 17:28:23 +0300

    Semi Full Menu Code

--- Arduino Code/full_code(Ameer)/menu_code(Youssed_Edit).ino
+++ Arduino Code/full_code(Ameer)/menu_code(Youssed_Edit).ino
@@ -1,27 +1,40 @@
-#include "U8glib.h"
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
 #include "bitmaps.h"
 #include "application.h"
 #include "Multi_Metre_Sig.h"
-#include <stdint.h>
-#include <stdlib.h>
+//#include <stdint.h>
 
-U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0 | U8G_I2C_OPT_NO_ACK | U8G_I2C_OPT_FAST); // Fast I2C / TWI
-// U8GLIB_SSD1306_128X64 u8g(13, 11, 8, 9, 10); // SPI connection
-// for SPI connection, use this wiring:
-// GND > GND
-// VCC > 5V
-// SCL > 13
-// SDA > 11
-// RES > 10
-// DC > 9
-// CS > 8
+// OLED display width and height, in pixels
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1        // Reset pin (set to -1 if sharing Arduino reset pin)
 
-char menu_items [6] [MAX_ITEM_LENGTH] = {
+// I2C address of the OLED display (usually 0x3C or 0x3D)
+#define SSD1306_I2C_ADDRESS 0xBC
+
+// Initialize the display object for I2C
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Define constants for text size and color
+#define TEXT_SIZE 1
+#define TEXT_COLOR WHITE
+
+// Function to draw a menu item
+void drawMenuItem(int y_position, const char* item_text, const uint8_t* item_bitmap) {
+    oled.setTextSize(TEXT_SIZE);
+    oled.setTextColor(TEXT_COLOR);
+    oled.setCursor(26, y_position);
+    oled.print(item_text);
+    oled.drawBitmap(3, y_position - 13, item_bitmap, 16, 16, TEXT_COLOR); // Adjusted y position for bitmap
+}
+
+char menu_items [NUM_ITEMS] [MAX_ITEM_LENGTH] = {
   {"Voltmeter"},
   {"Ammeter"},
   {"Ohmmeter"},
   {"Sig Gen"},
-  {"Scope"},
   {"Config"}
 };
 
@@ -35,8 +48,6 @@ char sig_menu_items [4] [MAX_ITEM_LENGTH] = {
 flag_type flags = { };
 
 //Global Variables
-uint8_t NUM_ITEMS = 6;
-
 menus selected_menu = MainMenu;
 
 uint8_t item_selected = 0; // which item in the menu is selected
@@ -44,17 +55,24 @@ uint8_t item_selected = 0; // which item in the menu is selected
 uint8_t item_sel_previous; // previous item - used in the menu screen to draw the item before the selected one
 uint8_t item_sel_next; // next item - used in the menu screen to draw next item after the selected one
 
-uint8_t device_selected = Voltmeter;
-uint8_t range_selected = range1;
-uint8_t mode_selected = DC_MODE;
+devices device_selected = Voltmeter;
+ranges  range_selected = range1;
+modes mode_selected = DC_MODE;
 
 float device_reading = 0;
 
 void setup()
 {
   MM_Init();
-
-  u8g.setColorIndex(1);  // set the color to white
+  // Start the display with the appropriate I2C address
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Stop here if allocation fails
+  }
+  oled.clearDisplay();
+  oled.display();  // Initial display update
+  // Set the text color to white
+  oled.setTextColor(WHITE);
 
   // define pins for buttons
   // INPUT_PULLUP means the button is HIGH when not pressed, and LOW when pressed
@@ -66,22 +84,6 @@ void setup()
 
 void loop() 
 {
-  // Updates number of items according to selected menu
-  switch (selected_menu)
-  {
-    case MainMenu:
-    NUM_ITEMS = 6;
-    break;
-    
-    case SigGenMenu:
-    NUM_ITEMS = 4;
-
-    case ConfigMenu:
-    NUM_ITEMS = 2;
-
-    default:
-    NUM_ITEMS = 1;
-  }
 /***********************************************************************************************/
         /*Multi-Meter Code*/
 Select_Mux(device_selected, range_selected);  //Choose device
@@ -101,9 +103,6 @@ switch (device_selected)
     device_reading = Read_Ohm(range_selected);
     break;
 }
-
-char reading_arr[MAX_ITEM_LENGTH];
-itoa(device_reading, reading_arr,10); //Change reading to str to be printed on screen
 
 /***************************************************************************************/
   //Selection Buttons
@@ -161,17 +160,14 @@ itoa(device_reading, reading_arr,10); //Change reading to str to be printed on s
           {
             case 0:
               selected_menu = VoltmeterMenu;
-              device_selected = Voltmeter;
               break;
 
             case 1:
               selected_menu = AmmeterMenu;
-              device_selected = Ammeter;
               break;
 
             case 2:
               selected_menu = OhmmeterMenu;
-              device_selected = Ohmeter;
               break;
 
             case 3:
@@ -227,30 +223,48 @@ itoa(device_reading, reading_arr,10); //Change reading to str to be printed on s
   {
     item_sel_next = 0;
   }
-
-/********************************************************************************/
-      /*Signal Generator Selection*/
-
-  if(selected_menu == SigGenMenu)
-  {
-    switch (item_selected)
-          {
-            case 0:
-            break;
-
-            case 1:
-            break;
-
-            case 2:
-            break;
-
-            case 3:
-            break;
-          }
-  }
   
 /**************************************************************************************************/
                 /*OLED Main Menu*/
+  /* OLED Main Menu */
+  oled.clearDisplay(); // Clear the display before drawing each frame
+  switch (selected_menu) {
+      case MainMenu:
+          // Selection box and scroll bar
+          oled.drawBitmap(0, 0, epd_bitmap_bg, SCREEN_WIDTH, SCREEN_HEIGHT, TEXT_COLOR);
+
+          // Draw Menu Items
+          drawMenuItem(15, menu_items[item_sel_previous], bitmap_arr[item_sel_previous]); // Menu Item 1
+          drawMenuItem(37, menu_items[item_selected], bitmap_arr[item_selected]); // Menu Item 2
+          drawMenuItem(59, menu_items[item_sel_next], bitmap_arr[item_sel_next]); // Menu Item 3
+          break;
+
+      case VoltmeterMenu:
+          // Voltmeter Menu code
+          break;
+
+      case AmmeterMenu:
+          // Ammeter Menu code
+          break;
+
+      case OhmmeterMenu:
+          // Ohmmeter Menu code
+          break;
+
+      case SigGenMenu:
+          // Selection box and scroll bar
+          oled.drawBitmap(0, 0, epd_bitmap_bg, SCREEN_WIDTH, SCREEN_HEIGHT, TEXT_COLOR);
+          // Signal Generator Menu code
+          break;
+
+      case ConfigMenu:
+          // Config Menu code
+          break;
+  }
+
+  oled.display(); // Update the display with the new content
+  delay(50); // Optional: Delay for stability or to control the refresh rate
+  /*
   u8g.firstPage();
   do
   {
@@ -273,35 +287,20 @@ itoa(device_reading, reading_arr,10); //Change reading to str to be printed on s
         //Menu Item 3
         u8g.setFont(u8g_font_7x14);
         u8g.drawStr(26,59, menu_items[item_sel_next]);
-        u8g.drawBitmapP(3, 46, 16/8, 16, bitmap_arr[item_sel_next]);
       break;
 
       case VoltmeterMenu:
-      
-      u8g.setFont(u8g_font_7x14B);
-      u8g.drawStr(26,37, "Voltmeter Reading");
-      u8g.setFont(u8g_font_7x14);
-      u8g.drawStr(26,59, reading_arr);
       //Voltmeter Menu code
 
       break;
 
       case AmmeterMenu:
-
-      u8g.setFont(u8g_font_7x14B);
-      u8g.drawStr(26,37, "Ammeter Reading");
-      u8g.setFont(u8g_font_7x14);
-      u8g.drawStr(26,59, reading_arr);
       //Ammeter Menu code
 
       break;
 
       case OhmmeterMenu:
       //Ohmmeter Menu Code
-      u8g.setFont(u8g_font_7x14B);
-      u8g.drawStr(26,37, "Ohmmeter Reading");
-      u8g.setFont(u8g_font_7x14);
-      u8g.drawStr(26,59, reading_arr);
 
       break;
 
@@ -311,17 +310,6 @@ itoa(device_reading, reading_arr,10); //Change reading to str to be printed on s
 
         
       //Signal Generator Menu Code
-      //Menu Item 1
-        u8g.setFont(u8g_font_7x14);
-        u8g.drawStr(26,15, sig_menu_items[item_sel_previous]);
-
-        //Menu Item 2
-        u8g.setFont(u8g_font_7x14B);
-        u8g.drawStr(26,37, sig_menu_items[item_selected]);
-
-        //Menu Item 3
-        u8g.setFont(u8g_font_7x14);
-        u8g.drawStr(26,59, sig_menu_items[item_sel_next]);
 
       break;
 
@@ -332,4 +320,5 @@ itoa(device_reading, reading_arr,10); //Change reading to str to be printed on s
     }
 
   } while(u8g.nextPage());
+  */
 }
